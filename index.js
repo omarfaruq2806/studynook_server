30034e1bee861ee484ffff854138760355855110
omarfaruk <mdomarfaruq2806@gmail.com>
2026-05-19 21:47:16 +0600

    project started

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const env = require("dotenv");
+const cors = require("cors");
+
+const port = process.env.PORT || 8000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("Server is running on port 8000");
+});
